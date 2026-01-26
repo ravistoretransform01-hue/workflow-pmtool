ffd1a1bc89a1ed28ec1d74aa40444dd411d6a18a
kunal <kunal.storetransform@gmail.com>
2026-01-26 17:26:32 +0530

    fix(header): disable Members menu item click handler
    - Replace setUserManagementOpen(true) callback with empty function
    - Prevent unintended user management panel from opening
    - Maintain menu item visibility while disabling functionality

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -75,7 +75,7 @@ export function Header() {
               <User className="mr-2 h-4 w-4" />
               <span>My profile</span>
             </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setUserManagementOpen(true)}>
+            <DropdownMenuItem onClick={() => {}}>
               <Users className="mr-2 h-4 w-4" />
               <span>Members</span>
             </DropdownMenuItem>
