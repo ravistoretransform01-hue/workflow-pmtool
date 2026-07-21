e7c54fc009513ea94488d5daae97c23f25e7fdf7
ravistoretransform-web <ravistoretransform@gmail.com>
2026-07-21 13:33:06 +0530

    Revert "Fix TS build error unused boardIdNum"
    This reverts commit a3886836b3f40d556a4423e54d028332a96d2823.

--- src/features/workload/components/WorkloadBoard.tsx
+++ src/features/workload/components/WorkloadBoard.tsx
@@ -2820,6 +2820,8 @@ export function WorkloadBoard({
 
   const handlePersonChange = async (taskId: string, memberIds: string[], skipToast?: boolean) => {
     try {
+      const boardIdNum = Number(boardId);
+
       // Find the reference task to identify which member was added or removed
       let referenceTask: any = null;
       for (const group of groups) {
